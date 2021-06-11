1d92039627a69e4bf601eb007362a3565d26d69b
Pinkaew Horputra <pinkaew.hor@dome.tu.ac.th>
2021-06-11 09:10:31 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const Hapi = require('@hapi/hapi');
+const Vision = require('@hapi/vision');
+const Inert = require('@hapi/inert');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+const { jsontransform } = require('./views/helpers/jsontransform')
+
+const start = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+    
+    const swaggerOptions = {
+        info: {
+            title: "Test API",
+            version: Pack.version
+        }
+    }
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: './views',
+        layoutPath: './views/layouts',
+        helpersPath: './views/helpers'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, h) {
+            return h.view('index', { title: 'Github Search API' });
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/jsontransform',
+        options: {
+            handler: (request, h) => {
+                const payload = request.payload;
+                return jsontransform();
+            },
+            tags: ['api']
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri)
+
+    process.on('unhandleRejection', (err) => {
+        console.log(err);
+        process.exit(1);
+    });
+}
+
+start();
---
+++ views/helpers/jsontransform.js
@@ -0,0 +1,26 @@
+exports.jsontransform = (data) => {
+    let arr = []
+    var parent = []
+
+    for (const level in data) {
+        // count index of nodes in each level
+        let index = 0;
+        for (const item of data[level]) {
+            let prev_level = level - 1;
+            // link id to level index
+            parent[item.id] = index;
+            if (item.parent_id !== null) {
+                let parent_index = parent[item.parent_id];
+                // find number of children of its parent node
+                let n = Object.keys(data[prev_level][parent_index].children).length;
+                data[prev_level][parent_index].children[n] = item;
+                index++
+            }
+        }
+    }
+
+    arr = data[0];
+    return arr;
+}
+
+
